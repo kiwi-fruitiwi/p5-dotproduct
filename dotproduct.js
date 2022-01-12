c541c136a2f9c2d13c4c2b261847fb4855e04b50
kiwi-fruitiwi <zane.tian@gmail.com>
2022-01-12 09:01:19 -0800

    convert to fat arrows. add dashed line normal

--- dotproduct.js
+++ dotproduct.js
@@ -47,7 +47,7 @@ function vectorProjection(a, b) {
 
 function draw() {
     background(234, 34, 24)
-    strokeWeight(3)
+    strokeWeight(5)
     stroke(0, 0, 70)
 
     let origin = new p5.Vector(100, 200)
@@ -57,20 +57,24 @@ function draw() {
     a = new p5.Vector.sub(mouse, origin)
 
     // draw a line from our origin to vectors a and b
-    line(origin.x, origin.y, origin.x+a.x, origin.y+a.y)
-    line(origin.x, origin.y, origin.x+b.x, origin.y+b.y)
+    drawArrow(origin, createVector(origin.x+a.x, origin.y+a.y))
+    drawArrow(origin, createVector(origin.x+b.x, origin.y+b.y))
 
     let v = vectorProjection(a, b)
-    stroke(201, 96, 83) // blue
-    line(origin.x, origin.y, origin.x+v.x, origin.y+v.y)
+    stroke(201, 96, 83, 75) // blue
+    strokeWeight(3)
+    drawArrow(origin, createVector(origin.x+v.x, origin.y+v.y))
 
     // draw the normal
     strokeWeight(1)
+    drawingContext.setLineDash([5, 5]);
     stroke(0, 0, 80)
 
     // base of normal vector
-    let base = new p5.Vector(origin.x+v.x, origin.y+v.y)
-    drawArrow(base, mouse)
+    // let base = new p5.Vector(origin.x+v.x, origin.y+v.y)
+    line(origin.x+v.x, origin.y+v.y, mouseX, mouseY)
+
+    drawingContext.setLineDash([]);
 }
 
 
